c2aa45cce6df9b17dae392c1de38e48cf73b2e49
pereyrago <pereyrago@gmail.com>
2023-09-20 00:34:23 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-    assets: true,
-  },
-});
+export default defineConfig({});
